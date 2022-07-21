f49421c7cba1b9a1dd6a4e05bebea3af80296977
gilmo135 <109759208+gilmo135@users.noreply.github.com>
2022-07-21 20:08:24 +0100

    Create initial_test.py

---
+++ initial_test.py
@@ -0,0 +1,81 @@
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import argparse
+import imutils
+import time
+import cv2
+import os
+
+OpenCV Object Tracking
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", type=str,
+	help="path to input video file")
+ap.add_argument("-t", "--tracker", type=str, default="kcf",
+	help="OpenCV object tracker type")
+args = vars(ap.parse_args())
+
+
+# extract the OpenCV version info
+(major, minor) = cv2.__version__.split(".")[:2]
+# if we are using OpenCV 3.2 OR BEFORE, we can use a special factory
+# function to create our object tracker
+if int(major) == 3 and int(minor) < 3:
+	tracker = cv2.Tracker_create(args["tracker"].upper())
+# otherwise, for OpenCV 3.3 OR NEWER, we need to explicity call the
+# approrpiate object tracker constructor:
+else:
+	# initialize a dictionary that maps strings to their corresponding
+	# OpenCV object tracker implementations
+	OPENCV_OBJECT_TRACKERS = {
+		"csrt": cv2.TrackerCSRT_create,
+		"kcf": cv2.TrackerKCF_create,
+		"boosting": cv2.TrackerBoosting_create,
+		"mil": cv2.TrackerMIL_create,
+		"tld": cv2.TrackerTLD_create,
+		"medianflow": cv2.TrackerMedianFlow_create,
+		"mosse": cv2.TrackerMOSSE_create
+	}
+	# grab the appropriate object tracker using our dictionary of
+	# OpenCV object tracker objects
+	tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
+# initialize the bounding box coordinates of the object we are going
+# to track
+initBB = None
+
+
+#############This is main bit changed in code################
+vs = VideoStream(src=0).start()
+time.sleep(1.0)
+  
+# loop over frames from the video stream
+while True:
+	# grab the current frame, then handle if we are using a
+	# VideoStream or VideoCapture object
+	frame = vs.read()
+	frame = frame[1] if args.get("video", False) else frame
+	# check to see if we have reached the end of the stream
+	if frame is None:
+		break
+	# resize the frame (so we can process it faster) and grab the
+	# frame dimensions
+	frame = imutils.resize(frame, width=500)
+	(H, W) = frame.shape[:2]
+  
+ 
+	# check to see if we are currently tracking an object
+	if initBB is not None:
+		# grab the new bounding box coordinates of the object
+		(success, box) = tracker.update(frame)
+		# check to see if the tracking was a success
+		if success:
+			(x, y, w, h) = [int(v) for v in box]
+      os.system("echo Track Success | wall -n")
+      os.system("echo x, y, w, h | wall -n")
+      
+
+        
+        
+        
+
